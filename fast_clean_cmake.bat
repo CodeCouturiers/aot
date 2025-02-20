5505dc2d715ca48ac7985718ec916087fe4fe749
CodeCouturiers <info@codecouturehub.com>
2025-02-19 23:42:15 -0800

    update cmake code

--- fast_clean_cmake.bat
+++ fast_clean_cmake.bat
@@ -1,11 +1,15 @@
 @echo off
 echo Cleaning CMake cache and rebuilding...
 
+:: Set environment variables
+set "VSCMD_START_DIR=%CD%"
+
 :: Kill Visual Studio processes if they are running
 taskkill /F /IM devenv.exe /T 2>nul
 timeout /t 2 /nobreak >nul
 
 :: Remove build directories and CMake cache
+if exist out rmdir /s /q out
 if exist build rmdir /s /q build
 if exist CMakeCache.txt del /f /q CMakeCache.txt
 if exist CMakeSettings.json del /f /q CMakeSettings.json
@@ -14,5 +18,14 @@ if exist .vs rmdir /s /q .vs
 :: Create build directory and run CMake
 mkdir build
 cd build
+
+:: Configure CMake for Visual Studio 2019 with Win32
+cmake .. -G "Visual Studio 16 2019" -A Win32 ^
+    -DCMAKE_GENERATOR_PLATFORM=Win32 ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DBUILD_WINDOWS_GUI=ON ^
+    -DBUILD_DICTS=ON
+
 echo Done! Visual Studio solution has been regenerated.
+cd ..
 pause 
